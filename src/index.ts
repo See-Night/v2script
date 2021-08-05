fe9cda30a602a4d501f016ee4f7a06da354d7f3c
See-Night <wangshibo142@gmail.com>
2021-08-05 10:24:39 +0800

    FIXED

--- src/index.ts
+++ src/index.ts
@@ -75,7 +75,8 @@ export class v2sub {
         let res = request('GET', url);
         let body = res.getBody().toString();
         let vmesses: string[] = base64decode(body).toString().replace(/\n\n/g, '').replace(/vmess:\/\//g, '').split('\n');
-        for (let i = 0; i < vmesses.length; i++) {
+        console.log(vmesses);
+        for (let i = 0; i < vmesses.length - 1; i++) {
             let q = JSON.parse(base64decode(vmesses[i]).split('\r\n').join(''));
             this.subs.push(new sub(q.ps, q.add, q.port, q.id, q.aid, q.net));
         }
